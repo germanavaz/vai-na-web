91151d42540c254272975f407b2d94a984aaa30c
germanavaz <gmsvaz@gmail.com>
2025-01-17 15:28:34 -0300

    mudanças feitas na aula

--- aula-aladin/src/App.jsx
+++ aula-aladin/src/App.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
+import lampada from "./assets/lampada.png";
+import genio from "./assets/genio.png";
+import "./Style.scss";
 
 
 export default function App() {
+
+  const[aladin, setAladin] = useState(lampada);
+
+  const mudarImagem = () => {
+    setAladin(genio)
+  }
+
+
   return(
-    <>
+    <main>
     <h1>"Liberte a magia da programação com a lâmpada de Aladim."</h1>
-    
-    </>
+    <img src={aladin} alt="Imagem do gênio da lâmpada" />
+    <button onClick={mudarImagem}>Clique Aqui</button>
+    </main>
   )
 }

3bb430f7073a8a07ee54ef3d111a717a44948f2c
germanavaz <gmsvaz@gmail.com>
2025-01-23 09:40:54 -0300

    first commit

---
+++ aula-projeto-sinal/src/App.jsx
@@ -0,0 +1,13 @@
+import './Style.scss';
+
+export default function App() {
+  return(
+    <main>
+      <div className='sinal'>
+        <div className="luz"></div>
+        <div className="luz"></div>
+        <div className="luz"></div>
+      </div>
+    </main>
+  )
+}

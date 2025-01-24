355c13a09361dcd491eb833aca6a1d097d96ce95
germanavaz <gmsvaz@gmail.com>
2025-01-24 15:30:29 -0300

    aula api intro

--- aula-api-intro/src/App.jsx
+++ aula-api-intro/src/App.jsx
@@ -4,10 +4,26 @@ import axios from 'axios';
 
 
 export default function App() {
+
+  const [desenho, setDesenho] = useState([]);
+
+  const pegarDados = async () => {
+
+    const dados = await axios.get('https://api.sampleapis.com/rickandmorty/characters')
+    setDesenho(dados.data[2])
+  }
+
+  useEffect(()=>{
+    pegarDados()
+  }, [])
+
   return(
     <>
     <h1>API</h1>
     <h2>Application Programing Interface /  Interface de Programação de Aplicações</h2>
+    <article>
+      <img src={desenho.image} alt=""/>
+    </article>
 
     </>
   )

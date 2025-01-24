908f68d9342b364cbb1740a218859dc4bbe0d723
germanavaz <gmsvaz@gmail.com>
2025-01-24 14:46:12 -0300

    first commit

---
+++ aula-api-intro/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
+
+
+export default function App() {
+  return(
+    <>
+    <h1>API</h1>
+    <h2>Application Programing Interface /  Interface de Programação de Aplicações</h2>
+
+    </>
+  )
+}

22435196125bc91febe9bb4dd1493798d40cd1ef
germanavaz <gmsvaz@gmail.com>
2025-01-31 19:42:04 -0300

    aula python

---
+++ aula-python/main.py
@@ -0,0 +1,52 @@
+# python: snake case - ex: primeiro_nome, nome_do_vizinho, etc
+
+# tipos de dados
+# int
+idade = 28
+temperatura = -10
+
+# float
+altura = 1.68
+graus = -2.2
+
+# string(str)
+frase = "Boa noite!!!"
+
+# boolean
+tem_salario = False
+
+# nulo
+resultado = None
+
+# não definido
+# dia
+# print(dia)
+
+# Exibir soma
+primeiro_valor = 2.2
+segundo_valor = 2.1
+print(primeiro_valor + segundo_valor)
+
+# Exibir frase
+nome = 'Germana'
+sobrenome = 'Vaz'
+print('O nome é', nome, sobrenome)
+
+
+idade = 20
+nome = 'Germana'
+
+print(type(idade), type(nome))
+
+# --------------------
+
+nome = input("Qual o seu nome?")
+print('Olá,', nome)
+
+# -------------
+
+primeira_nota = input('Digite sua primeira nota:')
+segunda_nota = input('Digite sua segunda nota:')
+soma_notas = int(primeira_nota) + int(segunda_nota)
+
+print(f"A soma das suas notas é: {soma}")

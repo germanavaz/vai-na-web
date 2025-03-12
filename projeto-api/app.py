0d06c492ffd18f56498a9123b79d917787b3f3a6
germanavaz <gmsvaz@gmail.com>
2025-03-12 19:33:53 -0300

    add método post api

--- projeto-api/app.py
+++ projeto-api/app.py
@@ -1,6 +1,6 @@
-# no terminal: python app.py
+# no terminal: source venv/Scripts/activate, e depois: python app.py
 
-from flask import Flask
+from flask import Flask, request, jsonify
 import sqlite3
 
 app = Flask(__name__)
@@ -24,6 +24,27 @@ init_db()
 @app.route('/')
 def home_page():
     return '<h2>Minha página com Flask!</h2>'
+
+
+@app.route('/doar', methods=['POST'])
+def doar():
+    dados = request.get_json()
+
+    titulo = dados.get('titulo')
+    categoria = dados.get('categoria')
+    autor = dados.get('autor')
+    imagem_url = dados.get('imagem_url')
+
+    if not titulo or not categoria or not autor or not imagem_url:
+        return jsonify({"erro": "Todos os campos são obrigatórios."}), 400
+
+    with sqlite3.connect('database.db') as conn:
+        conn.execute(f""" INSERT INTO livros (titulo,categoria,autor,imagem_url)
+                     VALUES('{titulo}','{categoria}','{autor}','{imagem_url}')
+                     """)
+        conn.commit()
+
+        return jsonify({"mensagem": "Livro cadastrado com sucesso."}), 201
 
 
 if __name__ == '__main__':
